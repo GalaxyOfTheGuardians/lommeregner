098733427bde280a1a7d06b546e09b4e8e3308d5
Nikolaj Waldstrøm <nikolajwaldstrom@Nikolajs-MBP.edu.ats.dk>
2023-01-13 14:20:25 +0100

    Initial commit

---
+++ test/Enandenfil.py
@@ -0,0 +1,6 @@
+def Fil2 ():
+    print('det virker')
+
+def enFunktionMedParametre (førsteparameter, Δ)
+    print(sum(førsteparameter,Δ))
+
---
+++ test/main.py
@@ -0,0 +1,9 @@
+
+Hello = 6
+
+Good = "hello world"
+print('Hello')
+print(Good)
+print(str(Hello) + Good)
+
+
